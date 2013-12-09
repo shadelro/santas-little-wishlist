2d703c1efee2fb6e642c0cc361383940348ea824
Brent Shadel <brent.shadel@cerner.com>
2013-12-09 15:55:48 -0600

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+SantasLittleWishlist::Application.config.secret_key_base = '1b074fd83c2a58b787190885aa14442450ae77d4e69c4dbb39635c31bf94080bc2e5da54c6039409784b56c81ad8b5ef1fcdbd0c2dec40e84037934687969e76'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+SantasLittleWishlist::Application.config.session_store :cookie_store, key: '_santas-little-wishlist_session'
